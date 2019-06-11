79e78bb2cd1806111917077790d3f0e5ab941f2f
Ralf Junghanns <ralf.junghanns@gmail.com>
2019-06-11 22:22:15 +0200

    Add Pegel Pirna to readinput

--- public/readInput.php
+++ public/readInput.php
@@ -47,4 +47,35 @@ foreach ($validFiles as $filename) {
         );
 }
 
+$stations = [
+    [
+        'projectId' => 'DEU1',
+        'sensorId' => 'WL-ELBE-PIRNA',
+        'url' => 'https://www.pegelonline.wsv.de/webservices/rest-api/v2/stations/PIRNA/W.json?includeCurrentMeasurement=true'
+    ]
+];
+
+foreach ($stations as $station) {
+    $response = json_decode(file_get_contents($station['url']), true);
+    $dateTime = new DateTime($response['currentMeasurement']['timestamp']);
+    $waterLevel = $response['currentMeasurement']['value'];
+    $gaugeZero = $response['gaugeZero']['value'];
+
+    /** @var Sensor $sensor */
+    $sensor = $entityManager->getRepository(Sensor::class)->findOneBy([
+        'project' => $station['projectId'],
+        'name' => $station['sensorId']
+    ]);
+
+    if (!$sensor instanceof Sensor) {
+        $sensor = new Sensor($station['projectId'], $station['sensorId'], '');
+    }
+
+    $sensorValue = new SensorValue($dateTime, ['waterLevel' => $waterLevel, 'gaugeZero' => $gaugeZero]);
+    $sensor->addValue($sensorValue);
+
+    $entityManager->persist($sensor);
+    $entityManager->flush();
+}
+
 echo 'DONE!';
